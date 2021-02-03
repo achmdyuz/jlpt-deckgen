1f722eeae060a5c66468d965a091440937534290
achmdyuz <achm4d.bat@gmail.com>
2021-02-03 07:03:45 +0700

    Rewrite the function to make it more external input friendly

--- deckgen.py
+++ deckgen.py
@@ -6,64 +6,63 @@ import time
 import random
 import csv
 
-# Declare the main function and call other function from here 
 def main():
-    get_input()
+    for user_input in get_input():
+        level = user_input['jlpt_level']
+        start = user_input['start']
+        end = user_input['end']
+        tags = user_input['tags']
+        filename = user_input['filename']
+        for kanji in get_kanji(level, start, end):
+            write_csv(kanji['character'], kanji['meaning'], kanji['kun'], kanji['on'], tags, filename)
 
-# Scrapping start from here
+
 def get_input():
-    # Get some data from user
-        get_input.jlpt_level = int(input("Enter the JLPT level do you studying (1: hardest, 2, 3, 4, 5: easiest): "))
-        get_input.start_page = 0
-        get_input.end_page = int(input("How many pages do you want to scrapping? "))
-        get_input.tags = "JLPT-N"+str(get_input.jlpt_level)
-        temp_filename = str(input("Please enter the CSV filename: "))
-        if temp_filename == '':
-            get_input.filename = "JLPTN"+str(get_input.jlpt_level)+".csv"
-        else:
-            get_input.filename = temp_filename
+    jlpt_level = int(input("Choose your JLPT level between 1 and 5 (E.g : enter 1 for JLPT-N1) "))
+    start_page = int(input("Enter first page you want to scrapping "))
+    end_page = int(input("Enter last page you want to scrapping "))
+    temp_filename = str(input("Please enter the output filename "))
+    if temp_filename == '':
+        filename = "JLPT-N"+str(jlpt_level)+".csv"
+    else:
+        filename = temp_filename+".csv"
+    temp_tags = str(input("Please enter the tag label of the Anki decks "))
+    if temp_tags == '':
+        tags = "JLPT-N"+str(jlpt_level)
+    else:
+        tags = temp_tags
 
-        get_kanji(jlpt_level = get_input.jlpt_level, start_page = get_input.start_page, end_page = get_input.end_page) 
-def get_kanji(jlpt_level, start_page, end_page):
-    # Get kanji, meaning, and reading from jisho with user input as parameter
-    while(start_page <= end_page):
-    
-        source_page = requests.get(f'https://jisho.org/search/%23kanji%20%23jlpt-n{jlpt_level}?page={start_page}').text # Mix url source with data from user
-        start_page += 1
-        print("Scrapping page : "+str(start_page))
+    dict_input = {'jlpt_level':jlpt_level, 'start':start_page, 'end':end_page, 'tags':tags,'filename':filename}
+    yield dict_input
+
+def get_kanji(level, start, end):
+    while(start <= end):
+        source_page = requests.get(f'https://jisho.org/search/%23kanji%20%23jlpt-n{level}?page={start}').text
+        start += 1
+        print(f'Scrapping page: {str(start)}')
         delay = random.randint(1, 5)
         time.sleep(delay)
         soup = BeautifulSoup(source_page, 'lxml')
-    
-        #kanji = soup.find("a", attrs={"href":"jisho.org/search/%E6%B0%8F%20%23kanji"}) # This is how to find tag with attributes
 
-        # Output filename will be have name from variable below
-        nametag = "JLPT-N"+str(jlpt_level)
-        
-    
-        kanjis = soup.find_all('div', class_='entry kanji_light clearfix')
-        for kanji in kanjis:
-    
+        get_kanji = soup.find_all('div', class_='entry kanji_light clearfix')
+        for kanji in get_kanji:
             character = kanji.find('span', class_='character literal japanese_gothic').text.replace('\n', '').strip()
             meaning = kanji.find('div', class_='meanings english sense').text.replace('\n', '').strip()
             get_kun_reading = kanji.find('div', class_='kun readings')
             if get_kun_reading is None:
-                kun_reading = " "
+                kun_reading = ''
             else:
                 kun_reading = kanji.find('div', class_='kun readings').text.replace('\n', '').strip()
             get_on_reading = kanji.find('div', class_='on readings')
             if get_on_reading is None:
-                on_reading = " "
+                on_reading = ''
             else:
                 on_reading = kanji.find('div', class_='on readings').text.replace('\n', '').strip()
-            write_csv(character, meaning, kun_reading, on_reading, tags = nametag, this_file = nametag)
+            yield {'character':character, 'meaning':meaning,'kun':kun_reading, 'on':on_reading}
 
-# Put everything into CSV file
-def write_csv(character, meaning, kun_reading, on_reading, tags, this_file):
-    ''' This function used by script for writing csv file after receive all the data needed from get_kanji function'''
-    ''' To using this functon, call this function with parameter like this : write_dev(character, meaning, kun_reading, on_reading, tags, this_file) '''
-    with open(f'file_output/{this_file+".csv"}', 'a') as csv_source:
+def write_csv(character, meaning, kun, on, tags, filename):
+    with open(f'output_file/{filename}', 'a') as csv_source:
         csv_writer = csv.writer(csv_source, delimiter=':', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([character, meaning, kun_reading, on_reading, "Compound 0", "Compound 1", "Compound 2", "Compound 3", "Compound 4", "Sentences example : ", tags])
+        csv_writer.writerow([character, meaning, kun, on, tags])
 if __name__=='__main__':
     main()
