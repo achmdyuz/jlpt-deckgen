eb9afd2dc80a247ccb01d88d37de4dd9a2fed783
achmdyuz <achm4d.bat@gmail.com>
2021-02-04 15:51:02 +0700

    Fixing my first bug :)

--- modules/Writeit.py
+++ modules/Writeit.py
@@ -8,6 +8,6 @@ def write_csv(character, meaning, kun, on, tags, filename):
     path0.mkdir(exist_ok=True)
     with open(f'output_file/{filename}', 'a') as csv_source:
         csv_writer = csv.writer(csv_source, delimiter=':', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([character, meaning, kun, on, tags])
+        csv_writer.writerow([character, meaning, kun, on, 'Compound 0', 'Compound 1', 'Compound 2', 'Compound 3', 'Compound 4', 'Sentences examples', tags])
 if __name__=='__main__':
     main()
