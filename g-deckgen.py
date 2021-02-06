9def5a7a62948af83560a0613925743876847144
Achmdyuz <achmdyuz@users.noreply.github.com>
2021-02-06 08:50:03 +0700

    Try to following PEP8 standard by using flake8 in both g-deckgen and deckgen

--- g-deckgen.py
+++ g-deckgen.py
@@ -5,50 +5,78 @@ from tkinter import ttk
 from modules.Kanji import get_kanji
 from modules.Writeit import write_csv
 
+
 def scrapping():
     level = jlptchoosen.get()[6]
     if level == '1':
-        level = int(level) 
+        level = int(level)
         start = int('0')
         end = int('64')
-        tags = 'JLPT-N'+str(level)
-        filename = 'JLPT-N'+str(level)+'.csv'
+        tags = 'JLPT-N' + str(level)
+        filename = 'JLPT-N' + str(level) + '.csv'
         for kanji in get_kanji(level, start, end):
-            write_csv(kanji['character'], kanji['meaning'], kanji['kun'], kanji['on'], tags, filename)
+            write_csv(kanji['character'],
+                      kanji['meaning'],
+                      kanji['kun'],
+                      kanji['on'],
+                      tags,
+                      filename)
     elif level == '2':
-        level = int(level) 
+        level = int(level)
         start = int('0')
         end = int('21')
-        tags = 'JLPT-N'+str(level)
-        filename = 'JLPT-N'+str(level)+'.csv'
+        tags = 'JLPT-N' + str(level)
+        filename = 'JLPT-N' + str(level) + '.csv'
         for kanji in get_kanji(level, start, end):
-            write_csv(kanji['character'], kanji['meaning'], kanji['kun'], kanji['on'], tags, filename)
+            write_csv(kanji['character'],
+                      kanji['meaning'],
+                      kanji['kun'],
+                      kanji['on'],
+                      tags,
+                      filename)
     elif level == '3':
-        level = int(level) 
+        level = int(level)
         start = int('0')
         end = int('21')
-        tags = 'JLPT-N'+str(level)
-        filename = 'JLPT-N'+str(level)+'.csv'
+        tags = 'JLPT-N' + str(level)
+        filename = 'JLPT-N' + str(level) + '.csv'
         for kanji in get_kanji(level, start, end):
-            write_csv(kanji['character'], kanji['meaning'], kanji['kun'], kanji['on'], tags, filename)
+            write_csv(kanji['character'],
+                      kanji['meaning'],
+                      kanji['kun'],
+                      kanji['on'],
+                      tags,
+                      filename)
     elif level == '4':
-        level = int(level) 
+        level = int(level)
         start = int('0')
         end = int('12')
-        tags = 'JLPT-N'+str(level)
-        filename = 'JLPT-N'+str(level)+'.csv'
+        tags = 'JLPT-N' + str(level)
+        filename = 'JLPT-N' + str(level) + '.csv'
         for kanji in get_kanji(level, start, end):
-            write_csv(kanji['character'], kanji['meaning'], kanji['kun'], kanji['on'], tags, filename)
+            write_csv(kanji['character'],
+                      kanji['meaning'],
+                      kanji['kun'],
+                      kanji['on'],
+                      tags,
+                      filename)
     elif level == '5':
-        level = int(level) 
+        level = int(level)
         start = int('0')
         end = int('7')
-        tags = 'JLPT-N'+str(level)
-        filename = 'JLPT-N'+str(level)+'.csv'
+        tags = 'JLPT-N' + str(level)
+        filename = 'JLPT-N' + str(level) + '.csv'
         for kanji in get_kanji(level, start, end):
-            write_csv(kanji['character'], kanji['meaning'], kanji['kun'], kanji['on'], tags, filename)
+            write_csv(kanji['character'],
+                      kanji['meaning'],
+                      kanji['kun'],
+                      kanji['on'],
+                      tags,
+                      filename)
     else:
         print("Invalid choice")
+
+
 # Main window
 window = tkinter.Tk()
 window.title("Anki CSV Deck Generator")
@@ -56,9 +84,9 @@ window.title("Anki CSV Deck Generator")
 # Widgets
 
 # Combobox
-ttk.Label(window, text = "Select the JLPT level :")
+ttk.Label(window, text="Select the JLPT level :")
 select = tkinter.StringVar()
-jlptchoosen = ttk.Combobox(window, width = 35, textvariable = select)
+jlptchoosen = ttk.Combobox(window, width=35, textvariable=select)
 jlptchoosen['values'] = ('JLPT-N1',
                          'JLPT-N2',
                          'JLPT-N3',
@@ -70,7 +98,10 @@ jlptchoosen.pack()
 jlptchoosen.current(4)
 
 # Button
-clickthis = tkinter.Button(text = "Scrap it", width = 7, height = 2, command = scrapping)
+clickthis = tkinter.Button(text="Scrap it",
+                           width=7,
+                           height=2,
+                           command=scrapping)
 clickthis.pack()
 
 # Main window loop
