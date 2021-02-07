09c8dceefa68194028227193b2aea8367f8fad67
achmdyuz <achm4d.bat@gmail.com>
2021-02-07 14:33:10 +0700

    Adding line break to following PEP8?

--- modules/Kanji.py
+++ modules/Kanji.py
@@ -8,7 +8,8 @@ import random
 
 def get_kanji(level, start, end):
     while(start <= end):
-        url = (f'https://jisho.org/search/%23kanji%20%23jlpt-n{level}?page={start}')
+        url = (f'https://jisho.org/search/%23kanji%20%23jlpt-n{level}?\
+                 page={start}')
         source_page = requests.get(url).text
         start += 1
         print(f'Scrapping page: {str(start)}')
@@ -18,18 +19,23 @@ def get_kanji(level, start, end):
 
         get_kanji = soup.find_all('div', class_='entry kanji_light clearfix')
         for kanji in get_kanji:
-            character = kanji.find('span', class_='character literal japanese_gothic').text.replace('\n', '').strip()
-            meaning = kanji.find('div', class_='meanings english sense').text.replace('\n', '').strip()
+            character = kanji.find('span',
+                                   class_='character literal japanese_gothic')\
+                .text.replace('\n', '').strip()
+            meaning = kanji.find('div', class_='meanings english sense')\
+                .text.replace('\n', '').strip()
             get_kun_reading = kanji.find('div', class_='kun readings')
             if get_kun_reading is None:
                 kun_reading = ''
             else:
-                kun_reading = kanji.find('div', class_='kun readings').text.replace('\n', '').strip()
+                kun_reading = kanji.find('div', class_='kun readings')\
+                    .text.replace('\n', '').strip()
             get_on_reading = kanji.find('div', class_='on readings')
             if get_on_reading is None:
                 on_reading = ''
             else:
-                on_reading = kanji.find('div', class_='on readings').text.replace('\n', '').strip()
+                on_reading = kanji.find('div', class_='on readings')\
+                    .text.replace('\n', '').strip()
             yield {'character': character,
                    'meaning': meaning,
                    'kun': kun_reading,
